9819373c7123193eed6c3030aa8be60c2461b33d
mcfangpy <mcfangpy@gmail.com>
2023-04-07 15:15:09 +0800

    修改launchpad steps

--- apps/web/src/views/Ifos/components/IfoSteps.tsx
+++ apps/web/src/views/Ifos/components/IfoSteps.tsx
@@ -73,14 +73,14 @@ const Step1 = ({ hasProfile }: { hasProfile: boolean }) => {
     <Box>
       <Text>
         {t(
-          'The number of iCAKE equals the locked staking amount if the staking duration is longer than %weeks% weeks. If the staking duration is less than %weeks% weeks, it will linearly decrease based on the staking duration.',
+          'The number of iVE equals the locked staking amount if the staking duration is longer than %weeks% weeks. If the staking duration is less than %weeks% weeks, it will linearly decrease based on the staking duration.',
           {
             weeks: weeksDisplay,
           },
         )}
       </Text>
       <InlineLink external href="https://docs.pancakeswap.finance/products/ifo-initial-farm-offering/icake">
-        {t('Learn more about iCAKE')}
+        {t('Learn more about iVE')}
       </InlineLink>
     </Box>,
     {},
@@ -90,20 +90,20 @@ const Step1 = ({ hasProfile }: { hasProfile: boolean }) => {
     <CardBody>
       {tooltipVisible && tooltip}
       <Heading as="h4" color="secondary" mb="16px">
-        {t('Lock CAKE in the CAKE pool')}
+        {t('Lock VE in the VE pool')}
       </Heading>
       <Box>
         <Text mb="4px" color="textSubtle" small>
           {t(
-            'The maximum amount of CAKE you can commit to the Public Sale equals the number of your iCAKE. Lock more CAKE for longer durations to increase the maximum CAKE you can commit to the sale.',
+            'The maximum amount of VE you can commit to the Public Sale equals the number of your iVE. Lock more VE for longer durations to increase the maximum VE you can commit to the sale.',
           )}
         </Text>
         <TooltipText as="span" fontWeight={700} ref={targetRef} color="textSubtle" small>
-          {t('How does the number of iCAKE calculated?')}
+          {t('How does the number of iVE calculated?')}
         </TooltipText>
         <Text mt="4px" color="textSubtle" small>
           {t(
-            'Missed this IFO? You will enjoy the same amount of iCAKE for future IFOs if your locked-staking position is not unlocked.',
+            'Missed this LaunchPad? You will enjoy the same amount of iVE for future LaunchPad if your locked-staking position is not unlocked.',
           )}
         </Text>
       </Box>
@@ -114,7 +114,7 @@ const Step1 = ({ hasProfile }: { hasProfile: boolean }) => {
               <LogoRoundIcon style={{ alignSelf: 'flex-start' }} width={32} height={32} />
               <Box ml="16px">
                 <Text bold fontSize="12px" textTransform="uppercase" color="secondary">
-                  {t('Your max CAKE entry')}
+                  {t('Your max VE entry')}
                 </Text>
                 <Balance fontSize="20px" bold decimals={5} value={getBalanceNumber(credit)} />
                 <Text fontSize="12px" color="textSubtle">
@@ -145,17 +145,17 @@ const Step2 = ({ hasProfile, isLive, isCommitted }: { hasProfile: boolean; isLiv
   return (
     <CardBody>
       <Heading as="h4" color="secondary" mb="16px">
-        {t('Commit CAKE')}
+        {t('Commit VE')}
       </Heading>
       <Text color="textSubtle" small>
         {t(
-          'Please note that CAKE in the fixed-term staking positions will remain locked and can not be used for committing to IFO sales. You will need a separate amount of CAKE in your wallet balance to commit to the IFO sales.',
+          'Please note that VE in the fixed-term staking positions will remain locked and can not be used for committing to LaunchPad sales. You will need a separate amount of VE in your wallet balance to commit to the LaunchPad sales.',
         )}{' '}
         <br />
       </Text>
       {hasProfile && isLive && !isCommitted && (
         <Button as="a" href="#current-ifo" mt="16px">
-          {t('Commit CAKE')}
+          {t('Commit VE')}
         </Button>
       )}
     </CardBody>
@@ -216,7 +216,7 @@ const IfoSteps: React.FC<React.PropsWithChildren<TypeProps>> = ({
               {t('Activate your Profile')}
             </Heading>
             <Text color="textSubtle" small mb="16px">
-              {t('You’ll need an active PancakeSwap Profile to take part in an IFO!')}
+              {t('You’ll need an active ViewExchange Profile to take part in an LaunchPad!')}
             </Text>
             {renderAccountStatus()}
           </CardBody>
@@ -233,7 +233,7 @@ const IfoSteps: React.FC<React.PropsWithChildren<TypeProps>> = ({
             </Heading>
             <Text color="textSubtle" small>
               {t(
-                'After the IFO sales finish, you can claim any IFO tokens that you bought, and any unspent CAKE tokens will be returned to your wallet.',
+                'After the LaunchPad sales finish, you can claim any LaunchPad tokens that you bought, and any unspent VE tokens will be returned to your wallet.',
               )}
             </Text>
           </CardBody>
@@ -246,7 +246,7 @@ const IfoSteps: React.FC<React.PropsWithChildren<TypeProps>> = ({
   return (
     <Wrapper>
       <Heading id="ifo-how-to" as="h2" scale="xl" color="secondary" mb="24px" textAlign="center">
-        {t('How to Take Part in the Public Sale')}
+        How to Take Part in the LaunchPad
       </Heading>
       <Stepper>
         {stepsValidationStatus.map((_, index) => (
